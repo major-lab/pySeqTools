66cebd7091f54de1c7e534899e685027aaec2d4e
nweill <naweill@gmail.com>
2014-07-21 10:51:44 -0400

    Warning, work in progress

--- ScanSeed.py
+++ ScanSeed.py
@@ -1,4 +1,5 @@
 from script.seqTools import *
+from script.ppPrint import *
 
 __author__ = 'nath'
 
@@ -15,10 +16,9 @@ def scan(mRNA,miR):
     listHits = re.finditer(seed_rc,mRNA.seq)
     A1 = False
     m8 = False
-    seedType = ""
     res = {}
     for m in listHits:
-        print (mRNA.seq[m.start()-1:m.end()+1])
+
         if mRNA.seq[m.end()] == 'A':
             A1 = True
         if mRNA.seq[m.start()-1] == revCompl(miR.seq[7]):
@@ -39,6 +39,20 @@ def scan(mRNA,miR):
         res[m.start()] = seedtype
     return res
 
+def getFPScan(seedtype):
+    res = [0,0,0,0]
+    if not seedtype:
+        return res
+    else:
+        res[0]=1
+        if seedtype == "7mer-A1":
+            res[1] = 1
+        elif seedtype == "7mer-m8":
+            res[2] = 1
+        else:
+            res = [1,1,1,1]
+
+    return res
 
 
 
@@ -49,5 +63,8 @@ if __name__ == '__main__':
     mir = fc.findSeq("MIMAT0000063","MIMAT")
     print (mir.seq)
     res = scan(mRNA,mir)
+    for pos in  res.keys():
+        doPPrint(mRNA.seq,mir.seq,pos,offsetmRNA=30)
+        print (getFPScan(res.get(pos)))
     print (res)
 
